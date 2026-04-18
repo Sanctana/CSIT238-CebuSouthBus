0a95df0496b64b3237d2c770bfed8d31727f8e8f
✿ Fleur de Blue <135421389+Xylphy@users.noreply.github.com>
2026-04-18 18:35:44 +0800

    feat: Navigation to About Us

---
+++ app/src/main/java/edu/cit/tangpos/bustransport/fragments/AboutUsFragment.kt
@@ -0,0 +1,18 @@
+package edu.cit.tangpos.bustransport.fragments
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import edu.cit.tangpos.bustransport.R
+
+class AboutUsFragment : Fragment(R.layout.fragment_aboutus){
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Handle the back button inside the About Us layout
+        view.findViewById<View>(R.id.topBar)?.setOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
+}
--- app/src/main/java/edu/cit/tangpos/bustransport/fragments/AccountFragment.kt
+++ app/src/main/java/edu/cit/tangpos/bustransport/fragments/AccountFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import edu.cit.tangpos.bustransport.R
 import androidx.core.content.edit
 import edu.cit.tangpos.bustransport.database.DBHelper
@@ -18,6 +19,11 @@ class AccountFragment : Fragment(R.layout.fragment_account) {
         val usernameField = view.findViewById<TextView>(R.id.username)
 
         val btnLogOut = view.findViewById<LinearLayout>(R.id.btnLogOut)
+        val btnAboutUs = view.findViewById<LinearLayout>(R.id.btnAboutUs)
+
+        btnAboutUs.setOnClickListener {
+            findNavController().navigate(R.id.nav_about_us)
+        }
 
         val sharedPreferences = requireContext().getSharedPreferences("Bus Transport", Context.MODE_PRIVATE)
         val userId = sharedPreferences.getString("userId", null)
